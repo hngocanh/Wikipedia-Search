16eb02fa42ce86bb93e65d774c591f9715d29ec3
Anh Hoang <108174327+hngocanh@users.noreply.github.com>
2023-07-05 22:28:27 +0700

    Delete SearchResultsPage.ts

--- Locators/SearchResultsPage.ts
+++
@@ -1,8 +0,0 @@
-// @ts-check
-export const searchResultsPage = {
-
-    SECOND_SEARCH_BAR: '//input[@id="ooui-php-1"]',
-    SECOND_SEARCH_BTN: '//button[@type="submit"]',
-    PAGE_HEADING: '#firstHeading'
-
-};
